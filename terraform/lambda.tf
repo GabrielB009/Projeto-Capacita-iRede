a3f8015c6af9c9a52abcbd1d4af0eacea3508726
Gabriel Braga Carneiro <gabrielbcarneiro1980@gmail.com>
2026-09-04 19:03:09 -0300

    automatiza empacotamento da Lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,15 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "consumer" {
   function_name = "${var.project_name}-consumer"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.handler"
   runtime       = "nodejs18.x"
 
-  filename         = "lambda.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
